634cef017bc1d2ec760ae719b2b90fcf6a819a87
Mur meow <saparova.qwer@gmail.com>
2023-10-24 14:19:19 +0500

    task one

---
+++ cooking_book.py
@@ -0,0 +1,70 @@
+import os
+
+# current_directory = os.getcwd()
+# print(current_directory)
+
+with open('recipes.txt', 'w', encoding='utf-8') as f:
+    f.write('''Омлет
+3
+Яйцо | 2 | шт
+Молоко | 100 | мл
+Помидор | 2 | шт
+
+Утка по-пекински
+4
+Утка | 1 | шт
+Вода | 2 | л
+Мед | 3 | ст.л
+Соевый соус | 60 | мл
+
+Запеченный картофель
+3
+Картофель | 1 | кг
+Чеснок | 3 | зубч
+Сыр гауда | 100 | г
+
+Фахитос
+5
+Говядина | 500 | г
+Перец сладкий | 1 | шт
+Лаваш | 2 | шт
+Винный уксус | 1 | ст.л
+Помидор | 2 | шт''')
+
+
+def cook_book():
+    cook_book = {}
+    with open('recipes.txt', encoding='utf-8') as f:
+        while True:
+            key = f.readline().strip()
+            if not key:
+                break
+            values_number = int(f.readline().strip())
+            ingredients = []
+            for i in range(values_number):
+                ingredient_line = f.readline().strip()
+                ingredient_parts = ingredient_line.split(' | ')
+                ingredient_name = ingredient_parts[0]
+                quantity = int(ingredient_parts[1])
+                measure = ingredient_parts[2]
+                ingredients.append({
+                    'ingredient_name': ingredient_name,
+                    'quantity': quantity,
+                    'measure': measure
+                })
+            f.readline()  
+
+            cook_book[key] = ingredients
+
+    return cook_book
+
+result = cook_book()
+for dish, ingredients in result.items():
+    print(dish)
+    for ingredient in ingredients:
+        print(ingredient)
+
+
+
+
+ 
